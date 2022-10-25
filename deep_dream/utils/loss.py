a9f06e50e8f9695ab6f56f03e089201f3528b800
Alessandro <alessandro1.sanvito@mail.polimi.it>
2022-10-26 00:08:31 +0200

    feat: add loss

---
+++ deep_dream/utils/loss.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+
+
+def calc_loss(img, model):
+    # Pass forward the image through the model to retrieve the activations.
+    # Converts the image into a batch of size 1.
+    img_batch = tf.expand_dims(img, axis=0)
+    layer_activations = model(img_batch)
+    if len(layer_activations) == 1:
+        layer_activations = [layer_activations]
+
+    losses = []
+    for act in layer_activations:
+        loss = tf.math.reduce_mean(act)
+        losses.append(loss)
+
+    return tf.reduce_sum(losses)
