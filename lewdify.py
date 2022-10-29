a7a580ec571984c305ad546587862264c0b13d4f
Alessandro Sanvito <alessandro.sanvito@gmail.com>
2022-10-29 22:42:01 +0200

    feat: support argparse

--- lewdify.py
+++ lewdify.py
@@ -1,9 +1,8 @@
-from deep_dream.deep_dream import DeepDream
 import argparse
-from pathlib import Path
-from typing import List
+import os
 
 import tensorflow as tf
+
 from private_detector.utils.preprocess import preprocess_for_evaluation
 
 
@@ -36,18 +35,45 @@ def read_image(filename: str) -> tf.Tensor:
 
 def lewdify(
         restore_path: str,
-        image_paths: List[str]
+        input_path: str,
+        output_path: str
 ) -> None:
     # TODO implement and test model loading (tf.saved_model.load(model) does not restore custom gradients :))
+    #images_names = os.listdir(input_path)
+    # images_paths = map(lambda name: os.path.join(input_path, name), images_names)
     # for image_path in image_paths:
     #    image = read_image(image_path)
+
+    os.makedirs(output_path, exist_ok=True)
     return
 
 
 if __name__ == '__main__':
-    # TODO support command line with argparse
     # TODO support lewd image generation from random noise
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--restore_path',
+        type=str,
+        help='Location of SavedModel to load',
+        default='saved_checkpoint/ckpt-0.9375-14.index'
+    )
+    parser.add_argument(
+        '--input_path',
+        type=str,
+        help='Paths to image paths to predict for',
+        default='sonny.jpg'
+    )
+    parser.add_argument(
+        '--output_path',
+        type=str,
+        help='Paths to image paths to predict for',
+        default='output'
+    )
+    args = parser.parse_args()
+
     lewdify(
-        restore_path='saved_checkpoint/ckpt-0.9375-14.index',
-        image_paths=['sonny.jpg']
+        restore_path=args.restore_path,
+        input_path=args.input_path,
+        output_path=args.output_path
     )
